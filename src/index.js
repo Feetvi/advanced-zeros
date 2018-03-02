32947b8c528f082859082f5dfe10aa0cdfa69b78
Feetvi <razer-key@mail.ru>
2018-03-02 19:51:26 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var b = base;
+  var num = number;
+  var pow = 0;
+  var res = 0;
+
+  do {  	
+  	base = base / 2;
+  	pow++;  	
+  } while (base / 2 >= 1); 
+  	
+  for (var i = 2; i < num; i *= 2) {
+  //	console.log(i);
+  	res += Math.floor(num / i);
+ // 	console.log(res); 
+  }
+
+//  console.log(pow);
+  res = Math.floor(res / pow);
+
+  return res;
 }
+
+
+//console.log(getZerosCount(100, 16));
